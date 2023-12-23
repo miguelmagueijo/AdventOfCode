9896213b4c380490a558715259d9d6a0588a6fc6
Miguel Magueijo <miguelmagueijo@hotmail.com>
2023-12-23 13:14:14 +0000

    Day 1: Add part 2

--- 2023/day_1/main.py
+++ 2023/day_1/main.py
@@ -1,14 +1,13 @@
 from pathlib import Path
 
-def calibrate(lines: list[str]) -> int:
-    numbers = range(0, 10)
+def part1(lines: list[str]) -> int:
     line_numbers = list()
 
     for l in lines:
         first_number = second_number = 0
         found_first = False
         for c in l.strip():
-            if c.isnumeric() and int(c) in numbers:
+            if c.isnumeric():
                 if found_first:
                     second_number = c
                 else:
@@ -16,13 +15,35 @@ def calibrate(lines: list[str]) -> int:
                     found_first = True
         
         line_numbers.append(int(f"{first_number}{second_number}"))
-        print(line_numbers)
+
+    return sum(line_numbers)
+
+def part2(lines: list[str]) -> int:
+    line_numbers = list()
+    numbers_str = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    for l in lines:
+        numbers = list()
+        for i, c in enumerate(l.strip()):
+            if c.isnumeric():
+                numbers.append(c)
+                continue
+
+            for j, n_str in enumerate(numbers_str):
+                if l[i:].startswith(n_str):
+                    numbers.append(j)
+
+        line_numbers.append(int(f"{numbers[0]}{numbers[-1]}"))
 
     return sum(line_numbers)
 
 
+
 if __name__ == "__main__":
-    with open(Path(__file__).with_name("input.txt"), "r") as fp:
+    base_path = Path(__file__)
+
+    with open(base_path.with_name("input.txt"), "r") as fp:
         file_lines = fp.readlines()
 
-        print(f"Calibration total: {calibrate(file_lines)}")
+        print(f"Part 1 calibration total: {part1(file_lines)}")
+        print(f"Part 2 calibration total: {part2(file_lines)}")
